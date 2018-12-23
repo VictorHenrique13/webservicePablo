3dfce845bce25458246e8f6405e5e32e5f031dba
sherlock <victorhgm13@gmail.com>
2018-12-23 10:51:11 -0200

    Cadastar produto ainda com falha

--- app/src/main/java/br/com/viphost/kardenapp/VIEW/CadastrarProduto.java
+++ app/src/main/java/br/com/viphost/kardenapp/VIEW/CadastrarProduto.java
@@ -1,15 +1,16 @@
 package br.com.viphost.kardenapp.VIEW;
 
 import androidx.appcompat.app.AppCompatActivity;
-import br.com.viphost.kardenapp.R;
 
 import android.os.Bundle;
 
-public class CadastroProduto extends AppCompatActivity {
+import br.com.viphost.kardenapp.R;
+
+public class CadastrarProduto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro__produto);
+        setContentView(R.layout.cadastro_produto);
     }
 }
