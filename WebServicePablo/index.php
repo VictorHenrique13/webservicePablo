30bbda42dada2392a5bc5e1ae0ede0f78fb56639
victor <victorhgm13@gmail.com>
2019-02-10 09:47:17 -0200

    teste de webservice

---
+++ WebServicePablo/index.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manike
+ * Date: 04/10/18
+ * Time: 09:47
+ */
+    //error_reporting(0);
+    //ini_set('display_errors', 0 );
+    use \Psr\Http\Message\ServerRequestInterface as Request;
+    use \Psr\Http\Message\ResponseInterface as Response;
+
+    require 'vendor/autoload.php';
+
+
+    $app = new \Slim\App;
+    //header('Content-type: application/json');
+    /////////////////////////////////////////////////////////////////////////////////
+///
+
+    $app->get('/prod', function (Request $request, Response $response, array $args) {
+
+        $response->getBody()->write("Hello");
+
+        return $response;
+    });
+    $app->post('/imc/{imc}',function (Request $request, Response $response, array $args){
+        $sql = "INSERT INTO imc(peso,altura,data,idpaciente) VALUES(:peso,:altura,:data,:idpaciente);";
+        $imc = json_decode($request->getBody());
+        $conn = new PDO("mysql:dbname=pablofica;host=localhost","root","");
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(":peso",$imc->peso);
+        $stmt->bindParam(":altura",$imc->altura);
+        $stmt->bindParam(":data",$imc->data);
+        $stmt->bindParam(":idpaciente",$imc->idpaciente);
+        $stmt->execute();
+
+        echo json_encode($conn->lastInsertId());
+        
+    });
+
+
+    $app->run();
+
+
+
+
+
+
+?>
